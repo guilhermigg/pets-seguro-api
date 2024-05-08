ef14209eab82df53c2dc2e828f387c8a35c25c8d
guilhermigg <guilhermigg.e@gmail.com>
2024-05-08 09:11:02 -0300

    new: validação e swagger adicionados

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe)
+
+  const config = new DocumentBuilder()
+    .setTitle('Pets Seguro API')
+    .setDescription('API de gerenciamento de seguros de saúde para pets ;)')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
