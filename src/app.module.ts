6c53676c6f772525e63f79b9d4965c3a2dcc9862
guilhermigg <guilhermigg.e@gmail.com>
2024-05-08 09:13:05 -0300

    update: adicionando e atualizando imports no module geral

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaService } from './database/prisma.service';
+import { JwtModule } from "@nestjs/jwt"
+import { UserRepository } from './repositories/user_repository';
+import { PrismaUserRepository } from './repositories/prisma/prisma_user_repository';
+import { UserController } from './controllers/user.controller';
+import { BCryptService } from './auth/bcrypt.service';
+import { AuthService } from "./auth//auth.service";
+import { AuthController } from './controllers/auth.controller';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    JwtModule.register({
+        secret: process.env.JWT_SECRET,
+        signOptions: {expiresIn: '7d'}
+    })
+  ],
+  controllers: [
+    UserController,
+    AuthController
+  ],
+  providers: [
+    PrismaService,
+    AuthService,
+    BCryptService,
+    {
+      provide: UserRepository,
+      useClass: PrismaUserRepository
+    }
+  ],
 })
+
 export class AppModule {}
