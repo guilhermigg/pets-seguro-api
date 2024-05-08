f3194a3ffc541c38da5465c002fdace92657d95f
guilhermigg <guilhermigg.e@gmail.com>
2024-05-08 09:11:25 -0300

    new: dtos para payloads das requests

---
+++ src/dto/user.dto.ts
@@ -0,0 +1,25 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { user_role } from "@prisma/client";
+import { IsEmail, IsNotEmpty, IsStrongPassword, Length } from "class-validator";
+
+export class UserDTO {
+    @IsNotEmpty()
+    @Length(3, 80)
+    @ApiProperty()
+    name: string
+
+    @IsNotEmpty()
+    @IsEmail()
+    @ApiProperty()
+    email: string
+
+    @IsNotEmpty()
+    @Length(8, 120)
+    @ApiProperty()
+    @IsStrongPassword()
+    password: string
+
+    @IsNotEmpty()
+    @ApiProperty()
+    role: user_role 
+}
